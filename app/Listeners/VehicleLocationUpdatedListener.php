9347de61d3b503fa1bed0ca44cc3e0785bef91a9
Ahmed Hemida <Ahmed_Hemida>
2023-05-29 09:50:48 +0300

    add listen and event

---
+++ app/Listeners/VehicleLocationUpdatedListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\VehicleLocationUpdated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class VehicleLocationUpdatedListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\VehicleLocationUpdated  $event
+     * @return void
+     */
+    public function handle(VehicleLocationUpdated $event)
+    {
+        //
+    }
+}
